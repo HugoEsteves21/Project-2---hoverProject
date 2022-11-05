ee80ffb48c718f768c301d3230757e037e921860
Hugo Esteves <hugo.veigaa@gmail.com>
2022-11-05 20:51:50 +0000

    creating seeds file and creating file for the models

---
+++ bin/seeds.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Restaurant = require('../models/Restaurant.model');
+
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hoverProject';
+
+
+const restaurants = [
+    
+];
+
+
+async function seeds() {
+    try {
+      const x = await mongoose.connect(MONGO_URI);
+      console.log(`Connected to: ${x.connections[0].name}`);
+  
+      const createdRestaurants = await Book.create(restaurants);
+  
+      console.log(`Successfuly created ${createdRestaurants.length} restaurants`);
+  
+      x.disconnect();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  seeds();
