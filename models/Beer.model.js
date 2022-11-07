c57994d65ee2311d94a331fb7a5f20d9cabf116f
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2022-11-07 10:34:33 +0000

    finished updating beer model

--- models/Beer.model.js
+++ models/Beer.model.js
@@ -1,49 +1,54 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const beerSchema = new Schema(
-    {
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
     imageUrl: String,
     style: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     brewery: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
     },
     quantity: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
-    ABV: {
-        type: Number,
-        required: true,
-        trim: true,
+    abv: {
+      type: Number,
+      required: true,
+      trim: true,
     },
     brand: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     restaurantId: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Restaurant'
-        }],
-    },
-    {
-        // this second object adds extra properties: `createdAt` and `updatedAt`
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Restaurant",
+      },
+    ],
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
-    }
+  }
 );
 
-module.exports = model('Beer', beerSchema);
+module.exports = model("Beer", beerSchema);
